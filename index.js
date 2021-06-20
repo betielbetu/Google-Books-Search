45f22af63614d49ecd1b67e41a87ac0430ff07ba
Betiel <betyuser15@gmail.com>
2021-06-19 21:29:31 -0500

    heroku01

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', routes);
 app.use(express.static(path.join(__dirname,'client/build')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname , "/../client/build/index.html"));
+});
+app.use('/', routes);
+
 
 app.listen(port, function(){
 	console.log(`Express started on http://localhost:${port} - press Ctrl-C to exit`);

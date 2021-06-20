10de502d7cce6b8d85ef071bee3f7411a23f6ad7
Betiel <betyuser15@gmail.com>
2021-06-19 21:23:51 -0500

    finding correct path for heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname,'client/build')));
 
 app.listen(port, function(){
 	console.log(`Express started on http://localhost:${port} - press Ctrl-C to exit`);

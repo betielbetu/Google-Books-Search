380cc5ade45ad601df4d2577aa08187b0b8bbdd4
Betiel <betyuser15@gmail.com>
2021-06-19 21:02:33 -0500

    minor change for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require("body-parser");
 const path = require('path');
-const port = 3001;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());

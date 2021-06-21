65f6b28acfd5fc6732564ac544cb4645ebdf279b
Betiel <betyuser15@gmail.com>
2021-06-21 00:46:57 -0500

    readme update

---
+++ README.md
@@ -0,0 +1,38 @@
+# Google Books Search
+
+A React-based Google Books Search app. Users can search by the book title and would get a list, and are able to save their favorites. 
+
+## Technologies used 
+
+
+
+
+JavaScript, React, React-DOM, React-Scripts, Bootstrap, CSS, Google Books API, Axios, Express, MongoDB, Mongoose 
+
+
+
+
+## Screenshots of the application
+
+
+
+
+<img src="./client/public/images/pic2.png" width="1000" height="350">
+
+
+
+<img src="./client/public/images/pic1.png" width="1000" height="350">
+
+
+
+<img src="./client/public/images/pic.png" width="1000" height="350">
+
+
+
+
+## Submission 
+
+
+* The URL to the deployed application: https://fierce-beyond-40641.herokuapp.com
+
+* The URL to the GitHub repository: https://github.com/betielbetu/Google-Books-Search
---
+++ client/build/precache-manifest.f5fde8d05921cc346e2fbf8ef0aac007.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c1872643cd68e6cae5f834e525e975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19cc541c331602ad4b72",
+    "url": "/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "3213fc67268eda95fb32",
+    "url": "/static/css/main.71891e26.chunk.css"
+  },
+  {
+    "revision": "19cc541c331602ad4b72",
+    "url": "/static/js/2.2ecc3d85.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.2ecc3d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3213fc67268eda95fb32",
+    "url": "/static/js/main.d6cd8bca.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1f87e58f5fa40e11999dfee9b9ad21c5",
+    "url": "/static/media/header.1f87e58f.jpg"
+  }
+]);
